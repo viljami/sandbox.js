4baa229054ed1784d5e989d380eaed076a9a4cf0
Viljami Peltola <viljami.peltola@iki.fi>
2015-07-16 02:49:02 +1000

    dependency injection v 0.1.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,32 @@
 var gulp = require('gulp');
 var serve = require('gulp-serve');
+var ginject = require('gulp-inject');
+
+var scripts = [
+  './lab/bird/scripts/**/*.js',
+  './lab/lib/**/*.js'
+];
+
+gulp.task('inject', function () {
+  return gulp.src('./lab/bird/index.html')
+  .pipe(ginject(
+    gulp.src(scripts, {read: false}), {
+      transform: function(filepath){
+        return [
+          '<script src="',
+          filepath.replace('lab/', ''),
+          '"></script>'
+        ].join('');
+      }
+    })
+  )
+  .pipe(gulp.dest('./lab/bird'));
+});
 
 gulp.task('default', serve({
-  root: ['lessons', 'bower_components'],
+  root: ['lessons', 'bower_components', 'lab'],
   port: 3000
 }));
-gulp.task('serve', ['default']);
+gulp.task('serve', ['default', 'inject']);
+
+
